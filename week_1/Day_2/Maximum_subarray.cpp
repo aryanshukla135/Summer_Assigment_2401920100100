46ee7dcd2599a033aae591109f3102af96e85b90
aryanshukla135 <aryanshukla135@gmail.com>
2026-06-02 09:08:08 +0530

    Create Maximum_subarray.cpp

---
+++ week_1/Day_2/Maximum_subarray.cpp
@@ -0,0 +1,20 @@
+//tc -> O(n)
+//sc -> O(1)
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        int curr =0;
+        int maxsum = INT_MIN;
+
+        for(int i =0 ; i< n ; i++){
+            curr+=nums[i];
+            maxsum = max(maxsum,curr);
+            if(curr<0){
+                curr=0;
+            }
+            
+        }
+        return maxsum ;
+    }
+};

9107ec31b3ff546453609870a5bbb2ab00c8111d
aryanshukla135 <aryanshukla135@gmail.com>
2026-06-02 09:10:16 +0530

    Create Contain_Duplicate.cpp

---
+++ week_1/Day_2/Contain_Duplicate.cpp
@@ -0,0 +1,17 @@
+//tc -> O(n)
+//sc->O(n)
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+         int n = nums.size();
+         unordered_map<int,int> m ;
+         for(int i =0 ; i< n ; i++){
+            m[nums[i]]++;
+
+            if(m[nums[i]]>=2){
+                return true ;
+            }
+         }
+         return false ;
+    }
+};

77c0c05af368404ede30b4bf723298e9fb98e772
aryanshukla135 <aryanshukla135@gmail.com>
2026-06-03 08:24:29 +0530

    Create Move_Zeroes.cpp

---
+++ week_1/Day_3/Move_Zeroes.cpp
@@ -0,0 +1,15 @@
+//tc ->O(n)
+//sc->O(1)
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i = 0 ;
+        int n = nums.size();
+        for(int j = 0 ; j< n ;j++){
+            if(nums[j]!=0){
+                swap(nums[i],nums[j]);
+                i++;
+            }
+        }
+    }
+}; 

aa4aeb0bb4f5e96ac4a79af73c43581a73c67bc5
aryanshukla135 <aryanshukla135@gmail.com>
2026-06-10 08:40:45 +0530

    Create Repeated_Substring_Pattern.cpp

---
+++ week_2/Day_3/Repeated_Substring_Pattern.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        int n = s.length();
+        for(int l = n/2 ; l>=1;l--){
+            if(n%l==0){
+                int times= n/l;
+                string pattern = s.substr(0,l);
+                string ans = "";
+                while(times--){
+                    ans+=pattern;
+                }
+                if(ans==s){
+                    return true ;
+                }
+            }
+        }
+        return false ;
+    }
+};

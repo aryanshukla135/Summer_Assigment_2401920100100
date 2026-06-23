d8bf63cd116d7ca3d23791678d50a77e59bdb20b
aryanshukla135 <aryanshukla135@gmail.com>
2026-06-23 22:04:45 +0530

    Create Binary_Tree_inorder_traversal.cpp

---
+++ week_4/Day_2/Binary_Tree_inorder_traversal.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;
+        TreeNode*curr = root ;
+
+        while(curr != nullptr){
+             if(curr->left == nullptr){
+                result.push_back(curr->val);
+                curr = curr->right ;
+
+             }else{
+                TreeNode* leftNode = curr->left ;
+                while(leftNode->right != nullptr){
+                    leftNode = leftNode->right;
+                }
+                TreeNode* temp = curr;
+                leftNode->right = curr ;
+                curr = curr->left ;
+                temp ->left = nullptr;
+                
+             }
+        }
+        return result;
+    }
+};
